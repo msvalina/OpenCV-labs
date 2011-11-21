b59a644892d71e2b949756779763af11fd8b585c
msvalina <marijan.svalina@gmail.com>
2011-11-21 10:52:45 +0100

    dodani switchevi i video

--- lab1/edge_crop_video.cpp
+++ lab1/edge_crop_video.cpp
@@ -18,6 +18,8 @@ void crop_image( Mat& img, Rect rect );
 
 Rect box;
 bool drawing_box = false;
+bool croping_roi = false;
+bool running_video = false;
 
 int main(int argc, char** argv) {
 
@@ -41,48 +43,71 @@ int main(int argc, char** argv) {
     Mat temp;
     cvtColor( mat_image, gray_image, CV_RGB2GRAY );
     
-    Canny(gray_image, gray_image, 50, 200, 3);
-
-    namedWindow( imageName, CV_WINDOW_AUTOSIZE );
-    namedWindow( "Gray image", CV_WINDOW_AUTOSIZE );
-    
-    setMouseCallback( imageName, onMouse, (void*)&mat_image );
-
     while ( 1 ){
         mat_image.copyTo(temp);
-        if( drawing_box ){
-            draw_box( temp, box );
-        }
+        namedWindow( imageName, CV_WINDOW_AUTOSIZE );
         imshow( imageName, temp );
-        imshow( "Gray image", gray_image );
 
-        char c;
-        c = waitKey( 15 );
-        if( c == 27 ) break;
-    }
+        int c = waitKey(15);
+        switch( (char)c )
+        {
+            case 27:
+                cout << "Exiting ... \n ";
+                return 0;
+            case 'c':
+                cout << "Calling canny... \n";
+                Canny(gray_image, gray_image, 50, 200, 3);
+                
+                namedWindow( "Gray image", CV_WINDOW_AUTOSIZE );
+                imshow( "Gray image", gray_image );
+                break;
+            case 'r':
+                croping_roi = true;
+                running_video = false;
+                if( croping_roi ) {
+                    cout << "Setting callback, Image ROI i crop mode ...\n";
+                    setMouseCallback( imageName, onMouse, (void*)&mat_image );
+                if( drawing_box ) {
+                    draw_box( temp, box );
+                }
+                }
+                break;
+            case 'v':
+                cout << "Camera mode... \n"
+                    << "Destroying croping mode \n";
+                if ( croping_roi ) {
+                    destroyWindow( imageName );
+                    croping_roi = false;
+                }
+                running_video = true;
+                if( running_video ){
+                    VideoCapture cap(0);
+                    if( !cap.isOpened() ){
+                        cerr << "fail preko default camere \n";
+                        cout << "isprobavam argv2 " 
+                            << argv[2] << endl;
+                        cap.open( argv[2] );
+                        if( !cap.isOpened() ){
+                            cerr << "fail i preko argv[2] \n";
+                            return -1;
+                        }
 
-    /*
-    VideoCapture cap;
-    if (argc==1) {
-    	cap.open(0); // open the default camera
-    } else {
-    	cap.open(argv[1]);
-    }
-    if(!cap.isOpened())  // check if we succeeded
-    {
-    	cerr << "Couldn't open capture." << endl;
-        return -1;
-    }
+                    }
 
-    Mat frame;
-    while(1) {
-    	cap >> frame;
-   		if(!frame.data) break;
-        imshow( "Example2_9", frame );
-        char c = waitKey(10);
-        if( c == 27 ) break;
+                    Mat frame;
+                    while( 1 ){
+                        cap >> frame;
+                        if(!frame.data) break;
+                        namedWindow( "camera", CV_WINDOW_AUTOSIZE );
+                        imshow( "camera", frame );
+                        char c = waitKey(10);
+                        if( c == 'q' ) break;
+                    }
+
+                }
+                break;
+        }
     }
-    */
 
     return 0;
 }
@@ -103,19 +128,17 @@ void onMouse( int event, int x, int y, int flags, void* param ) {
     Mat& image = *(Mat*) param;
     switch( event )
     {
-        case CV_EVENT_LBUTTONDOWN:{
+        case CV_EVENT_LBUTTONDOWN:
             drawing_box = true;
             box = Rect(x, y, 0, 0);
-            }
             break;
-        case CV_EVENT_MOUSEMOVE: {
+        case CV_EVENT_MOUSEMOVE: 
             if( drawing_box ) {
                 box.width = x-box.x;
                 box.height = y-box.y;
             }
-            }
             break;
-        case CV_EVENT_LBUTTONUP: {
+        case CV_EVENT_LBUTTONUP: 
             drawing_box = false;
             if( box.width<0 ) {
                 box.x+=box.width;
@@ -131,7 +154,6 @@ void onMouse( int event, int x, int y, int flags, void* param ) {
                 << box.height << "\t" << box.width << "\n";
             //draw_box( image, box );
             crop_image( image, box);
-            }
             break;
     }
 } 
