b9b18ba226410679d36473f025e85de68f2ae22a
Marijan Svalina <marijan.svalina@gmail.com>
2012-01-26 17:08:14 +0100

    radi

--- lab4/feature_matching.cpp
+++ lab4/feature_matching.cpp
@@ -28,6 +28,7 @@ void matchTemplateOnCrop( int, void* );
 void getPoints( int event, int x, int y, int flags, void* param );
 void savePoint( int x, int y );
 void callHoughTransform( );
+void surfFlannMatcher( );
 
 Mat loaded_img, ss_img, cam_frame;
 Mat canny_roi, canny_gray, canny_detected_edges, canny_out; 
@@ -123,8 +124,12 @@ int main(int argc, char** argv) {
                 }
                 callHoughTransform( );
                 break;
+            case 'l':
+                surfFlannMatcher( );
+                break;
         }
     }
+
 
     return 0;
 }
@@ -298,11 +303,11 @@ void savePoint( int x, int y ){
 void getPoints( int event, int x, int y, int flags, void* param ) {
     switch( event ) {
         case CV_EVENT_LBUTTONDOWN:
-                break;
+            break;
         case CV_EVENT_LBUTTONUP:
-                savePoint( x, y );
-                break;
-         }
+            savePoint( x, y );
+            break;
+    }
 }
 
 void callHoughTransform( ){
@@ -316,7 +321,7 @@ void callHoughTransform( ){
     rho = rho_roi + pt1.x * cos( theta ) + pt1.y * sin( theta );
     cout << "rho_roi = " << rho_roi << endl;
     cout << "theta = " << theta << endl;
-    
+
     // ucitavanje parametara kamere
     FileStorage fs("calib/cam.xml", FileStorage::READ);
     Mat intrinsics(3, 3, CV_32F ); 
@@ -345,7 +350,7 @@ void callHoughTransform( ){
     // solvePnP mijenja Mat type u CV_32F te se elementima moram pristupat s .at<dobule>
     //cout << "rvec = " << rvec <<  endl;
     cout << "tvec = " << tvec <<  endl;
-    
+
     Rodrigues( rvec, R );
     //Mat t = tvec.t(); // transponirana matrica tvec 
     //cout << "t = " << t << endl;
@@ -355,7 +360,7 @@ void callHoughTransform( ){
 
     A = intrinsics * R; // A = P * R
     B = intrinsics * tvec; // B = P * t
-    
+
     //cout << "B = " << B <<  endl;
     //cout << "A = " << endl << " " << A << endl << endl;
     //cout << "A[1] = " << A.at<double>(0,0) << " A[1] = " << A.at<double>(0,1) << endl;
@@ -376,3 +381,66 @@ void callHoughTransform( ){
 
 }
 
+void surfFlannMatcher( ){
+    Mat img_1 = imread( global_argv[1], CV_LOAD_IMAGE_GRAYSCALE );
+    Mat img_2 = imread( global_argv[2], CV_LOAD_IMAGE_GRAYSCALE );
+    if( !img_1.data || !img_2.data )
+    { std::cout<< " --(!) Error reading images " << std::endl; }
+
+    //-- Step 1: Detect the keypoints using SURF Detector
+    int minHessian = 400;
+
+    SurfFeatureDetector detector( minHessian );
+
+    std::vector<KeyPoint> keypoints_1, keypoints_2;
+
+    detector.detect( img_1, keypoints_1 );
+    detector.detect( img_2, keypoints_2 );
+
+    //-- Step 2: Calculate descriptors (feature vectors)
+    SurfDescriptorExtractor extractor;
+
+    Mat descriptors_1, descriptors_2;
+
+    extractor.compute( img_1, keypoints_1, descriptors_1 );
+    extractor.compute( img_2, keypoints_2, descriptors_2 );
+
+    //-- Step 3: Matching descriptor vectors using FLANN matcher
+    FlannBasedMatcher matcher;
+    std::vector< DMatch > matches;
+    matcher.match( descriptors_1, descriptors_2, matches );
+
+    double max_dist = 0; double min_dist = 100;
+
+    //-- Quick calculation of max and min distances between keypoints
+    for( int i = 0; i < descriptors_1.rows; i++ )
+    { double dist = matches[i].distance;
+        if( dist < min_dist ) min_dist = dist;
+        if( dist > max_dist ) max_dist = dist;
+    }
+
+    printf("-- Max dist : %f \n", max_dist );
+    printf("-- Min dist : %f \n", min_dist );
+
+    //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist )
+    //-- PS.- radiusMatch can also be used here.
+    std::vector< DMatch > good_matches;
+
+    for( int i = 0; i < descriptors_1.rows; i++ )
+    { if( matches[i].distance < 2*min_dist )
+        { good_matches.push_back( matches[i]); }
+    }  
+
+    //-- Draw only "good" matches
+    Mat img_matches;
+    drawMatches( img_1, keypoints_1, img_2, keypoints_2, 
+            good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), 
+            vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS ); 
+
+    //-- Show detected matches
+    imshow( "Good Matches", img_matches );
+
+    for( int i = 0; i < good_matches.size(); i++ )
+    { printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx ); }
+
+}
