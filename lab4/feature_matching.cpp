b26c5479b3b910f627782c4a978cd2701f9e2bda
Marijan Svalina <marijan.svalina@gmail.com>
2013-09-25 16:04:48 +0200

    Changed variable name convection from foo_bar to fooBar

--- lab4/feature_matching.cpp
+++ lab4/feature_matching.cpp
@@ -6,17 +6,17 @@
 using namespace cv;
 using namespace std;
 
-void help(){
-    cout << "This code is in Public Domain, do what you like... " << endl;
-    cout << "Usage: ./binary <image_name> " << endl;
-    cout << "Hot keys: \n"
-                "\tr/R toggle croping \n"
-                "\tc/C toggle camera \n"
-                "\ts/S toggle taking screenshoot\n"
-                "\th/H toggle Hough Transform on screenshot\n"
-                "\te/E toggle edge detetction \n" 
-                "\tt/T toggle template matching\n";
-    }
+void 
+help(){
+cout << "Usage: ./binary <image_name> \n"
+"Hot keys: \n"
+"\tr/R toggle croping \n"
+"\tc/C toggle camera \n"
+"\ts/S toggle taking screenshoot\n"
+"\th/H toggle Hough Transform on screenshot\n"
+"\te/E toggle edge detetction \n" 
+"\tt/T toggle template matching\n";
+}
 
 void cannyEdge( Mat& img, Rect rect );
 void cannyTreshold( int, void* );
@@ -31,27 +31,27 @@ void savePoint( int x, int y );
 void callHoughTransform( );
 void surfFlannMatcher( );
 
-Mat loaded_img, ss_img, cam_frame;
-Mat canny_roi, canny_gray, canny_detected_edges, canny_out; 
-Mat croped_roi;
-Mat templ_img, result_img; 
+Mat loadedImg, ssImg, camFrame;
+Mat cannyRoi, cannyGray, cannyDetectedEdges, cannyOut; 
+Mat cropedRoi;
+Mat templImg, resultImg; 
 
 Point pt1, pt2, pt3, pt4;
 vector<Point2f> imagePoints(4);
 int n;
 Size2i size;
-Rect crop_box, canny_box, ss_box;
-bool drawing_box = false;
-char ** global_argv;
+Rect cropBox, cannyBox, ssBox;
+bool drawingBox = false;
+char ** globalArgv;
 int lowThreshold;
-int max_lowThreshold = 200;
+int maxLowThreshold = 200;
 int ratio = 3;
-int max_Trackbar = 5;
-int match_method = 5;
+int maxTrackbar = 5;
+int matchMethod= 5;
 
 int main(int argc, char** argv) {
     help();
-    global_argv = argv;
+    globalArgv = argv;
     
     if (argc < 2) {
         cout << " Usage: "<< argv[0] <<" <image> " << endl;
@@ -59,25 +59,23 @@ int main(int argc, char** argv) {
     }
 
     char* imageName = argv[1];
-    loaded_img = imread( imageName, CV_LOAD_IMAGE_COLOR);
+    loadedImg = imread( imageName, CV_LOAD_IMAGE_COLOR);
  
-    if( !loaded_img.data ) {
+    if( !loadedImg.data ) {
         cout << " Could not open or find the image" << endl;
         return -1;
     }
-    // dohvat width i height matrice
-    size = loaded_img.size();
-    //cout << size.width << " " << size.height << endl;
 
-    // postavljanje ROI na cijelu sliku
-    canny_box.x = 0;
-    canny_box.y = 0;
-    canny_box.width = size.width;
-    canny_box.height = size.height;
+    // set ROI on whole image
+    cannyBox.x = 0;
+    cannyBox.y = 0;
+    size = loadedImg.size();
+    cannyBox.width = size.width;
+    cannyBox.height = size.height;
 
     while ( 1 ){
         namedWindow( imageName, CV_WINDOW_AUTOSIZE );
-        imshow( imageName, loaded_img);
+        imshow( imageName, loadedImg);
 
         char c = waitKey(10);
         switch( c )
@@ -86,14 +84,14 @@ int main(int argc, char** argv) {
                 cout << "Exiting ... \n ";
                 return 0;
             case 'e':
-                cannyEdge( loaded_img, canny_box );
+                cannyEdge( loadedImg, cannyBox );
                 break;
             case 'E':
                 destroyWindow("canny");
                 break;
             case 'r':
                 cout << "Setting callback, calling cropImage  ...\n";
-                setMouseCallback( imageName, onMouse, (void*)&loaded_img );
+                setMouseCallback( imageName, onMouse, (void*)&loadedImg );
                 break;
             case 'R':
                 destroyWindow( "croped" );
@@ -102,7 +100,7 @@ int main(int argc, char** argv) {
                 initCamera( );
                 break;
             case 't':
-                if(!croped_roi.data){
+                if(!cropedRoi.data){
                     cout << "nisi cropao nista" << endl;
                     break;
                 }
@@ -116,10 +114,10 @@ int main(int argc, char** argv) {
                 destroyWindow( "snapshot" );
                 break;
             case 'h':
-                cannyEdge( ss_img, ss_box );
+                cannyEdge( ssImg, ssBox );
                 break;
             case 'H':
-                if(!canny_out.data){
+                if(!cannyOut.data){
                     cout << "pozivi canny" << endl;
                     break;
                 }
@@ -130,23 +128,21 @@ int main(int argc, char** argv) {
                 break;
         }
     }
-
-
     return 0;
 }
 
 void cannyTreshold( int, void* ){
-    Canny( canny_gray, canny_detected_edges, lowThreshold, lowThreshold*ratio, 3 );
-    canny_out = Scalar::all(0);
-    canny_detected_edges.copyTo( canny_out );
-    imshow( "canny", canny_out );
+    Canny( cannyGray, cannyDetectedEdges, lowThreshold, lowThreshold*ratio, 3 );
+    cannyOut = Scalar::all(0);
+    cannyDetectedEdges.copyTo( cannyOut );
+    imshow( "canny", cannyOut );
 }
 
 void cannyEdge( Mat& img, Rect rect ){
-    canny_roi = img( rect );
-    cvtColor( canny_roi, canny_gray, CV_RGB2GRAY);
+    cannyRoi = img( rect );
+    cvtColor( cannyRoi, cannyGray, CV_RGB2GRAY);
     namedWindow( "canny", CV_WINDOW_AUTOSIZE);
-    createTrackbar( "Min Treshold: ", "canny", &lowThreshold, max_lowThreshold, cannyTreshold );
+    createTrackbar( "Min Treshold: ", "canny", &lowThreshold, maxLowThreshold, cannyTreshold );
     cannyTreshold( 0, 0 );
 }
 
@@ -155,31 +151,31 @@ void onMouse( int event, int x, int y, int flags, void* param ) {
     switch( event )
     {
         case CV_EVENT_LBUTTONDOWN:
-            drawing_box = true;
-            crop_box = Rect(x, y, 0, 0);
+            drawingBox = true;
+            cropBox = Rect(x, y, 0, 0);
             break;
         case CV_EVENT_MOUSEMOVE: 
-            if( drawing_box ) {
-                crop_box.width = x-crop_box.x;
-                crop_box.height = y-crop_box.y;
+            if( drawingBox ) {
+                cropBox.width = x-cropBox.x;
+                cropBox.height = y-cropBox.y;
             }
             break;
         case CV_EVENT_LBUTTONUP: 
-            drawing_box = false;
-            if( crop_box.width<0 ) {
-                crop_box.x+=crop_box.width;
-                crop_box.width *= -1;
+            drawingBox = false;
+            if( cropBox.width<0 ) {
+                cropBox.x+=cropBox.width;
+                cropBox.width *= -1;
             }
-            if( crop_box.height<0 ) {
-                crop_box.y+=crop_box.height;
-                crop_box.height*=-1;
+            if( cropBox.height<0 ) {
+                cropBox.y+=cropBox.height;
+                cropBox.height*=-1;
             }
             cout << "box coordinates \n" 
                 << "x\t y\t height\t width\n"
-                << crop_box.x << "\t" << crop_box.y << "\t" 
-                << crop_box.height << "\t" << crop_box.width << "\n";
-            cropImage( image, crop_box);
-            //drawBox( image, crop_box );
+                << cropBox.x << "\t" << cropBox.y << "\t" 
+                << cropBox.height << "\t" << cropBox.width << "\n";
+            cropImage( image, cropBox);
+            //drawBox( image, cropBox );
             break;
     }
 } 
@@ -189,39 +185,40 @@ void drawBox( Mat& img, Rect rect ){
 }
 
 void cropImage( Mat& img, Rect rect ){
-    croped_roi = img( rect );
+    cropedRoi = img( rect );
     namedWindow( "croped", CV_WINDOW_AUTOSIZE );
-    imshow( "croped", croped_roi );
+    imshow( "croped", cropedRoi );
 }
 
 void initCamera( ){
-    cout << "Starting camera mode... \n";
+    cout << "Starting camera \n";
     VideoCapture cap(0);
     if( !cap.isOpened() ){
-        cerr << "fail preko default camere \n";
-        cout << "isprobavam argv[2] " 
-            << global_argv[2] << endl;
-        cap.open( global_argv[2] );
+        cerr << "Default camera won't open\n";
+        cout << "Trying argv[2] " 
+            << globalArgv[2] << endl;
+        cap.open( globalArgv[2] );
         if( !cap.isOpened() ){
-            cerr << "fail i preko argv[2] \n";
+            cerr << "Default camere isn't working\n";
+            cerr << "Try adding path to camera as argv[2]\n";
         }
     }
     bool camera=true;
     while( camera ){
-        cap >> cam_frame;
-        if(!cam_frame.data) break;
+        cap >> camFrame;
+        if(!camFrame.data) break;
         namedWindow( "camera", CV_WINDOW_AUTOSIZE );
-        imshow( "camera", cam_frame );
+        imshow( "camera", camFrame );
         char c = waitKey(10);
         switch( c ) {
             case 'o':
                 camera = false;
                 break;
             case 's':
-                cam_frame.copyTo( ss_img );
+                camFrame.copyTo( ssImg );
                 namedWindow( "snapshot", CV_WINDOW_AUTOSIZE );
-                imshow( "snapshot", ss_img );
-                cout << " Setting MouseCallback getPoints " << endl;
+                imshow( "snapshot", ssImg );
+                cout << " Setting MouseCallback on getPoints " << endl;
                 setMouseCallback( "snapshot", getPoints, 0 );
                 break;
         }
@@ -233,47 +230,47 @@ void matchTemplateTrackbar( ){
     namedWindow( "result", CV_WINDOW_AUTOSIZE );
 
     char* trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
-    createTrackbar( trackbar_label, "source" , &match_method, max_Trackbar, matchTemplateOnCrop );
+    createTrackbar( trackbar_label, "source" , &matchMethod, maxTrackbar, matchTemplateOnCrop );
 
     matchTemplateOnCrop( 0, 0 );
 }
 
 void matchTemplateOnCrop( int, void* ){
-    Mat source_img;
-    loaded_img.copyTo( source_img );
-    croped_roi.copyTo( templ_img );
+    Mat sourceImg;
+    loadedImg.copyTo( sourceImg );
+    cropedRoi.copyTo( templImg );
 
-    Mat gsource_img, gtempl_img;
-    cv::cvtColor(source_img, gsource_img, CV_BGR2GRAY);
-    cv::cvtColor(templ_img, gtempl_img, CV_BGR2GRAY);
+    Mat gsourceImg, gtemplImg;
+    cv::cvtColor(sourceImg, gsourceImg, CV_BGR2GRAY);
+    cv::cvtColor(templImg, gtemplImg, CV_BGR2GRAY);
     /// Create the result matrix
-    int result_cols =  source_img.cols - templ_img.cols + 1;
-    int result_rows = source_img.rows - templ_img.rows + 1;   
+    int result_cols =  sourceImg.cols - templImg.cols + 1;
+    int result_rows = sourceImg.rows - templImg.rows + 1;   
 
-    result_img.create( result_rows, result_cols, CV_32FC1 );
+    resultImg.create( result_rows, result_cols, CV_32FC1 );
 
     /// Do the Matching and Normalize
-    matchTemplate( gsource_img, gtempl_img, result_img, match_method);
-    normalize( result_img, result_img, 0, 1., NORM_MINMAX, -1, Mat() );
+    matchTemplate( gsourceImg, gtemplImg, resultImg, matchMethod);
+    normalize( resultImg, resultImg, 0, 1., NORM_MINMAX, -1, Mat() );
     // Remove non matching results with tresholding
-    threshold( result_img, result_img, 0.8, 1., THRESH_BINARY);
+    threshold( resultImg, resultImg, 0.8, 1., THRESH_BINARY);
 
     // Localizing the best match with minMaxLoc
     // Used only for testing purpose
     double minVal; double maxVal; double threshold=0.8;
     Point minLoc; Point maxLoc; Point matchLoc;
-    minMaxLoc( result_img, &minVal, &maxVal, &minLoc, &maxLoc);
-    rectangle( source_img, maxLoc, Point( maxLoc.x + templ_img.cols , maxLoc.y + templ_img.rows ), Scalar(0,0,255) ); 
+    minMaxLoc( resultImg, &minVal, &maxVal, &minLoc, &maxLoc);
+    rectangle( sourceImg, maxLoc, Point( maxLoc.x + templImg.cols , maxLoc.y + templImg.rows ), Scalar(0,0,255) ); 
 
     // Find all best matches and paint them
     // Go through every pixel from top left corner to right, top to down
-    for (int y = 1; y < result_img.rows -1; y++) {
-        for (int x = 1; x < result_img.cols -1; x++) {
+    for (int y = 1; y < resultImg.rows -1; y++) {
+        for (int x = 1; x < resultImg.cols -1; x++) {
             // search postion (y,x) but draw at (x,y) because pixels 
             // are showed difrently 
-            if (result_img.at<float>(y,x) > 0) {
-                cout << y << "," << x << " = " << result_img.at<float>(y,x) << endl; 
-                rectangle( source_img, Point(x,y), Point (x+templ_img.cols, y+templ_img.rows), Scalar(0,255,0));  
+            if (resultImg.at<float>(y,x) > 0) {
+                cout << y << "," << x << " = " << resultImg.at<float>(y,x) << endl; 
+                rectangle( sourceImg, Point(x,y), Point (x+templImg.cols, y+templImg.rows), Scalar(0,255,0));  
             }
         }
     }
@@ -282,8 +279,8 @@ void matchTemplateOnCrop( int, void* ){
     // for( it = I.begin<uchar>(), end = I.end<uchar>(); it != end; ++it)
     //*it = table[*it];
     // break;
-    imshow( "source", source_img );
-    imshow( "result", result_img);
+    imshow( "source", sourceImg );
+    imshow( "result", resultImg);
 }
 
 void savePoint( int x, int y ){
@@ -306,12 +303,12 @@ void savePoint( int x, int y ){
         pt4.x = x;
         pt4.y = y;
         imagePoints[3] = Point2f( x, y );
-        ss_box.x = pt1.x;
-        ss_box.y = pt1.y;
-        ss_box.width = pt4.x - ss_box.x;
-        ss_box.height = pt4.y - ss_box.y;
+        ssBox.x = pt1.x;
+        ssBox.y = pt1.y;
+        ssBox.width = pt4.x - ssBox.x;
+        ssBox.height = pt4.y - ssBox.y;
         cout << pt4.x << " " << pt4.y << endl;
-        cout << ss_box.width << " " << ss_box.height << endl;
+        cout << ssBox.width << " " << ssBox.height << endl;
     }
 }
 
@@ -327,14 +324,14 @@ void getPoints( int event, int x, int y, int flags, void* param ) {
 
 void callHoughTransform( ){
     vector<Vec2f> lines;
-    HoughLines( canny_out, lines, 1, CV_PI/180, 100, 0, 0 );
+    HoughLines( cannyOut, lines, 1, CV_PI/180, 100, 0, 0 );
     cout << "Lines = " << Mat( lines ) << endl;
-    float rho, rho_roi, theta;
-    rho_roi= lines[0][0];
+    float rho, rhoRoi, theta;
+    rhoRoi= lines[0][0];
     theta = lines[0][1];
     // prebacivanje u k.s. slike
-    rho = rho_roi + pt1.x * cos( theta ) + pt1.y * sin( theta );
-    cout << "rho_roi = " << rho_roi << endl;
+    rho = rhoRoi + pt1.x * cos( theta ) + pt1.y * sin( theta );
+    cout << "rhoRoi = " << rhoRoi << endl;
     cout << "theta = " << theta << endl;
 
     // ucitavanje parametara kamere
@@ -360,10 +357,8 @@ void callHoughTransform( ){
     Mat A( 3, 3, CV_32F );
     Mat B( 3, 1, CV_32F );
 
-    //cvFindExtrinsicCameraParams2() je zamjenjen s solvePnP()
     solvePnP( Mat(objectPoints), Mat(imagePoints), intrinsics, distortion, rvec, tvec, false );
     // solvePnP mijenja Mat type u CV_32F te se elementima moram pristupat s .at<dobule>
-    //cout << "rvec = " << rvec <<  endl;
     cout << "tvec = " << tvec <<  endl;
 
     Rodrigues( rvec, R );
@@ -380,7 +375,7 @@ void callHoughTransform( ){
     //cout << "A = " << endl << " " << A << endl << endl;
     //cout << "A[1] = " << A.at<double>(0,0) << " A[1] = " << A.at<double>(0,1) << endl;
 
-    double lambdaX, lambdaY, lambdaRo, rho_crtano, theta_crtano;
+    double lambdaX, lambdaY, lambdaRo, rhoCrtano, thetaCrtano;
     // lambdaX = a11*cos(theta) + a21*sin(theta) - ro*a31
     lambdaX = A.at<double>(0,0) * cos(theta) + A.at<double>(1,0) * sin(theta) - rho * (A.at<double>(2,0));
     // lambdaY = a12*cos(theta) + a22*sin(theta) - ro*a32
@@ -388,18 +383,18 @@ void callHoughTransform( ){
     // lamdbaRo = b3*ro - b1*cos(theta) - b2*sin(theta) 
     lambdaRo = rho * (B.at<double>(2)) - B.at<double>(0) * cos(theta) - B.at<double>(1) * sin(theta); 
 
-    theta_crtano = atan2( lambdaY, lambdaX );
-    rho_crtano = lambdaRo / sqrt( lambdaX * lambdaX + lambdaY * lambdaY );
+    thetaCrtano = atan2( lambdaY, lambdaX );
+    rhoCrtano = lambdaRo / sqrt( lambdaX * lambdaX + lambdaY * lambdaY );
 
-    cout << "Theta = " << theta_crtano*180/CV_PI << endl;
-    cout << "Rho = " << rho_crtano << endl;
+    cout << "Theta = " << thetaCrtano*180/CV_PI << endl;
+    cout << "Rho = " << rhoCrtano << endl;
 
 }
 
 void surfFlannMatcher( ){
-    Mat img_1 = imread( global_argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-    Mat img_2 = imread( global_argv[2], CV_LOAD_IMAGE_GRAYSCALE );
-    if( !img_1.data || !img_2.data )
+    Mat img1 = imread( globalArgv[1], CV_LOAD_IMAGE_GRAYSCALE );
+    Mat img2 = imread( globalArgv[2], CV_LOAD_IMAGE_GRAYSCALE );
+    if( !img1.data || !img2.data )
     { std::cout<< " --(!) Error reading images " << std::endl; }
 
     //-- Step 1: Detect the keypoints using SURF Detector
@@ -409,16 +404,16 @@ void surfFlannMatcher( ){
 
     std::vector<KeyPoint> keypoints_1, keypoints_2;
 
-    detector.detect( img_1, keypoints_1 );
-    detector.detect( img_2, keypoints_2 );
+    detector.detect( img1, keypoints_1 );
+    detector.detect( img2, keypoints_2 );
 
     //-- Step 2: Calculate descriptors (feature vectors)
     SurfDescriptorExtractor extractor;
 
     Mat descriptors_1, descriptors_2;
 
-    extractor.compute( img_1, keypoints_1, descriptors_1 );
-    extractor.compute( img_2, keypoints_2, descriptors_2 );
+    extractor.compute( img1, keypoints_1, descriptors_1 );
+    extractor.compute( img2, keypoints_2, descriptors_2 );
 
     //-- Step 3: Matching descriptor vectors using FLANN matcher
     FlannBasedMatcher matcher;
@@ -448,7 +443,7 @@ void surfFlannMatcher( ){
 
     //-- Draw only "good" matches
     Mat img_matches;
-    drawMatches( img_1, keypoints_1, img_2, keypoints_2, 
+    drawMatches( img1, keypoints_1, img2, keypoints_2, 
             good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), 
             vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS ); 
 
